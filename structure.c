a9d3060147df19088e33d2a117f6810b3de6d289
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-04-27 11:08:58 +0530

    Day 4 : Added 5 programs

---
+++ structure.c
@@ -0,0 +1,45 @@
+// #include<stdio.h>
+
+// // Declaration
+// #pragma pack(1)
+// struct Demo
+// {
+//   int i;      // 4
+//   char ch;    // 1   Generates padding
+//   float f;    // 4
+// };            // 9
+
+// int main()
+// {
+//   struct Demo dobj;
+
+
+//   dobj.i = 11;
+//   dobj.ch = 'A';
+//   dobj.f = 10.0f;
+
+//   printf("%d\n",dobj.i);    // 11
+//   printf("%c\n",dobj.ch);   // A
+//   printf("%f\n",dobj.f);    // 10.0
+
+
+//   return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//   int no;
+//   printf("Enter no:\n");
+//   scanf("%d",&no);
+//   if(no % 2 == 0)
+//   {
+//     printf("Even\n");
+//   }
+//   else{
+//     printf("Odd");
+//   }
+//   return 0;
+// }
+
