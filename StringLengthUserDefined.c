28478fd57aa4db99e1f9703462f99f2bfb3b2061
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-04-30 10:18:01 +0530

    Day 7: 3

---
+++ StringLengthUserDefined.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+//Call by address
+int strlenX(char *ptr)
+{
+  int i = 0;
+
+  while(*ptr != '\0')
+  {
+    i++;
+    ptr++;
+  }
+
+  return i;
+}
+
+int main()
+{
+ char str[] = "Ganesh";   //100
+ int ret = 0;
+
+ ret = strlenX(str);    //strlenX(100)
+ printf("String lenght : %d\n",ret);
+
+
+  return 0;
+
+  
+}
