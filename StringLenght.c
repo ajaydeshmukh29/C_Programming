bc63228373a465393fd711c4fcdfa939ba5bb0c7
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-04-30 10:16:09 +0530

    Day 7: 2

---
+++ StringLenght.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main()
+{
+ char str[] = "Ganesh";
+ int ret = 0;
+
+ ret = strlen(str);   // str give the base addresss of str
+ printf("String lenght : %d\n",ret);
+
+
+  return 0;
+
+  
+}
